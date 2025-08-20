63a4afb3a6f91543e9a4d9d539974abb7e46d12f
Paoloprimus <paolo.olivato@gmail.com>
2025-08-20 23:10:59 +0200

    Update openai.ts

--- lib/openai.ts
+++ lib/openai.ts
@@ -4,4 +4,4 @@ export const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY!,
 });
 
-export const LLM_MODEL = process.env.LLM_MODEL_NAME || "gpt-5";
+export const LLM_MODEL = process.env.LLM_MODEL_NAME || "gpt-4o-mini";
