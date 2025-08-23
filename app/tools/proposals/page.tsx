2e8ce16d2efede2359868f69019b36d4e58e222a
Paoloprimus <paolo.olivato@gmail.com>
2025-08-23 17:44:07 +0200

    Create page.tsx

---
+++ app/tools/proposals/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+import { ProposalGenerator } from '../../../components/ProposalGenerator';
+
+export default function Page() {
+  return (
+    <div className="p-6 max-w-3xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Generatore di Proposte</h1>
+      <ProposalGenerator />
+    </div>
+  );
+}
