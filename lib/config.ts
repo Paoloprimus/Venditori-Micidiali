2aa91a36a2a566ec66d28552173b6bbfca704eb0
Paoloprimus <paolo.olivato@gmail.com>
2025-10-26 07:52:27 +0100

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 export const USE_STANDARD_PHRASES = true;
 // lib/config.ts
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? 'Repping';
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? 'REPING';
